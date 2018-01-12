d7712b30e288cdac64ff69c8785385d42bb1931d
minesh <minesh.b.java@gmail.com>
2018-01-12 11:43:57 +0530

    fix warning

--- src/main/java/com/commons/auth/web/UserController.java
+++ src/main/java/com/commons/auth/web/UserController.java
@@ -1,8 +1,5 @@
 package com.commons.auth.web;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -10,7 +7,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
